f724d4d7b6172204e5af5f5b08a17bf42f400dce
raphaelletseng <33766912+raphaelletseng@users.noreply.github.com>
2017-11-18 16:42:13 -0500

    Added GUI file
    It's a gui!!

---
+++ codejam17.py
@@ -0,0 +1,48 @@
+from tkinter import *
+import pandas as pd
+#import matplotlib.pyplot as plt
+import pygal
+from PIL import Image, ImageTk
+
+#df = pd.read_csv('/Users/raphaelletseng/Desktop/Transportation/NYC General Transport/NYC-vehicle-collisions.csv')
+
+#print (df['BOROUGH'].value_counts())
+
+root = Tk()
+
+
+#matplotlib inline
+#df.boxplot(column="PERSONS INJURED", by ="BOROUGH")
+
+topFrame = Frame(root, width = 1000, height=1000)
+topFrame.pack()
+bottomFrame = Frame(root)
+bottomFrame.pack(side=BOTTOM)
+root.resizable(width=False, height=False)
+root.geometry('500x500')
+
+
+def button_pressed(borough):
+    print(borough)
+    label = Label(root, text=borough)
+    label.pack(side=TOP)
+    load = Image.open("/Users/raphaelletseng/Desktop/Screen Shot 2017-11-18 at 2.31.54 PM.png")
+    load = load.resize((500,400), Image.ANTIALIAS)
+    render = ImageTk.PhotoImage(load)
+    label_img = Label(root, image=render)
+    label_img.image = render
+    label_img.place(x=0, y=50)
+
+button1 = Button(topFrame, text ='Brooklyn', command = lambda: button_pressed('Brooklyn'))
+button2 = Button(topFrame, text = 'Queens', command = lambda: button_pressed('Queens'))
+button3 = Button(topFrame, text = 'Bronx',command = lambda: button_pressed('Bronx'))
+button4 = Button(topFrame, text = 'Manhattan',command = lambda: button_pressed ('Manhattan'))
+button5 = Button(topFrame, text = 'Staten Island',command = lambda: button_pressed ('Staten Island'))
+
+button1.pack(side=LEFT)
+button2.pack(side=LEFT)
+button3.pack(side=LEFT)
+button4.pack(side=LEFT)
+button5.pack(side=LEFT)
+
+root.mainloop()
