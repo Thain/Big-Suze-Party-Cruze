d990287d2eb16e6c0f02e19ff921bf54616906f4
Liam W Chung <33766900+lwalker43@users.noreply.github.com>
2017-11-18 20:03:18 -0500

    Delete getZip.py

--- getZip.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-import matplotlib as mpl
-
-def getZip(boro):
-    df = pd.read_csv("./NYC-vehicle-collisions.csv")
-    myBoro = df.loc[(df["BOROUGH"] == boro)]
-    deadZip = int(myBoro["ZIP CODE"].value_counts().idxmax())
-    return deadZip
