4b380951e78338fc65f603a963ef6be973af819c
lwalker43 <liam.chung@mail.mcgill.ca>
2017-11-19 10:01:59 -0500

    git cache cleared

--- Response.py
+++ Response.py
@@ -5,25 +5,43 @@ df = pd.read_csv("./NYC-vehicle-collisions.csv")
 def getBoro(boro):
 	maxZip = getMax('BOROUGH', boro)
 	maxNH = getNeighborhood(maxZip)
-	max_str1 = 'and the most accident prone zip code in your borough is ' + str(maxZip)
+	maxCas = getCasualties(maxZip)
+	max_str1 = 'and the most accident prone zip code is ' + str(maxZip) + ' with ' + maxCas + ' casualties.'
 	max_str2 = 'The most accident prone neighborhood in your borough is ' + str(maxNH)
 	
 	minZip = getMin('BOROUGH', boro)
 	minNH = getNeighborhood(minZip)
-	min_str1 = 'and the least accident prone zip code in your borough is ' + str(minZip)
+	minCas = getCasualties(minZip)
+	min_str1 = 'and the least accident prone zip code is ' + str(minZip) + ' with ' + minCas + ' casualties.'
 	min_str2 = 'The least accident prone neighborhood in your borough is ' + str(minNH)
 	
 	whole = max_str2 + '\n' + max_str1 + '\n' + min_str2 + '\n' + min_str1
 	return whole
 
+def getVehi(veh):
+	maxZip = getMax('VEHICLE', veh)
+	maxNH = getNeighborhood(maxZip)
+	maxCas = getCasualties(maxZip)
+	max_str1 = 'and the most accident prone zip code is ' + str(maxZip) + ' with ' + maxCas + ' casualties.'
+	max_str2 = 'The most accident prone neighborhood for your vehicle is ' + str(maxNH)
+	
+	minZip = getMin('VEHICLE', veh)
+	minNH = getNeighborhood(minZip)	
+	minCas = getCasualties(maxZip)
+	min_str1 = 'and the least accident prone zip code  is ' + str(minZip) + ' with ' + minCas + ' casualties.'
+	min_str2 = 'The least accident prone neighborhood for your vehicle is ' + str(minNH)
+	
+	whole = max_str2 + '\n' + max_str1 + '\n' + min_str2 + '\n' + min_str1
+	return whole
+
 def getMax(intype, inthing):
-	myBoro = df.loc[(df[intype] == inthing)]
-	maxZip = int(myBoro["ZIP CODE"].value_counts().idxmax())
+	myIn = df.loc[(df[intype] == inthing)]
+	maxZip = int(myIn['ZIP CODE'].value_counts().idxmax())
 	return maxZip
 
 def getMin(intype, inthing):
-	myBoro = df.loc[(df[intype] == inthing)]
-	minZip = int(myBoro["ZIP CODE"].value_counts().idxmin())
+	myIn = df.loc[(df[intype] == inthing)]
+	minZip = int(myIn['ZIP CODE'].value_counts().idxmin())
 	return minZip	
 		
 def getNeighborhood(zc):
@@ -34,9 +52,13 @@ def getNeighborhood(zc):
 	except KeyError:
 		deadNH = 'not found in our dictionary'
 	return deadNH
+
 def getCasualties(zc):
 	zipcas = df.loc[(df['ZIP CODE'] == zc)]
 	casualties = zipcas['PERSONS INJURED'].sum()
 	casualties += zipcas['PERSONS KILLED'].sum()
-	return casualties
+	if casualties != 0:
+		return str(casualties)
+	else:
+		return 'undocumented'
 	
